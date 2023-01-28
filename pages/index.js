0b77298f83d30d7b8e75a1f84d55870e7c7dc7f7
NahidBorson007 <123509058+NahidBorson007@users.noreply.github.com>
2023-01-28 06:25:38 +0600

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -11,7 +11,7 @@ export default function Home() {
       </Head>
 
       <main>
-        <Header title="Nahid Borson" />
+        <Header title="Welcome to my app!" />
         <p className="description">
           Get started by editing <code>pages/index.js</code>
         </p>
